0a0aabd969014dc5ec7fc374428c8ee09dc25b7e
Johannes Schüth <j.schueth@gentics.com>
2015-10-02 13:09:18 +0200

    Add mysql 5.5

---
+++ 5.5/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+service ssh start
+
+set -e
+TMPFS_DIR=/opt/tmpfs
+
+echo "Using tmpfs size: $TMPFS_SIZE"
+echo "none            $TMPFS_DIR  tmpfs   defaults,size=${TMPFS_SIZE}m,uid=999,gid=999,mode=0700          0       0" >> /etc/fstab
+
+echo "Move away old mysql data"
+mv $TMPFS_DIR /opt/backup
+
+echo "Recreate directory"
+mkdir $TMPFS_DIR  && chown mysql: $TMPFS_DIR
+
+echo "Mounting tmpfs and adding data"
+mount $TMPFS_DIR
+mv /opt/backup/* $TMPFS_DIR
+
+exec "eatmydata" "$@"
